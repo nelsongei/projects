b47f6cf25a4c7ac3889c40594987f2a3fb978af0
noslen254 <psammy.ngei@gmail.com>
2020-10-23 19:09:56 +0300

    Updating User registration::::

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
     public function index(){
-        return view('admin.index');
+        $users = User::orderBy('id','desc')->paginate(5);
+        return view('admin.index',compact('users'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -23,7 +23,39 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
-                        <div class="card-body"></div>
+                        <div class="card-body">
+                            <div class="col-md-12 text-left mb-3">
+                                <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#addNewUser">
+                                    <i class="fa fa-plus"></i>Add User
+                                </button>
+                            </div>
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Phone No.</th>
+                                    <th>Role</th>
+                                    <th>Action</th>
+                                </thead>
+                                <?php $count =1;?>
+                                <tbody>
+                                    @forelse($users as $user)
+                                        <tr>
+                                            <td>{{$count++}}</td>
+                                            <td>{{$user->name.' '.$user->lastName}}</td>
+                                            <td>{{$user->email}}</td>
+                                            <td>{{$user->phone}}</td>
+                                            <td>{{$user->role->role}}</td>
+                                            <td>
+                                                
+                                            </td>
+                                        </tr>
+                                    @empty
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
