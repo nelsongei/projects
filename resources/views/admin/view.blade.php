431f339d852901acac34868e97e5db8c4ae67893
noslen254 <psammy.ngei@gmail.com>
2020-10-23 20:20:59 +0300

    Added View file and made few changes

--- resources/views/admin/view.blade.php
+++ resources/views/admin/view.blade.php
@@ -35,7 +35,41 @@
                 <div class="row">
                     <div class="col-md-3">
                         <div class="card card-outline card-white">
-
+                            <div class="card-body box-profile">
+                                <div class="text-center">
+                                    @if(!empty($user['image']))
+                                        <img src="{{asset('')}}storage/{{$user->image}}" class="img-rounded profile-user-img img-circle" alt="user">
+                                    @else
+                                        <img src="{{asset('resources/settings/admin.png')}}" class="img-circle profile-user-img" alt="user">
+                                    @endif
+                                    <h3 class="profile-username">{{$user->name.' '.$user->lastName}}</h3>
+                                        <p class="text-blue">
+                                            {{$user->role->role}}
+                                        </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card card-white">
+                            <div class="card-header">
+                                <h3 class="card-title">More About {{$user->name}}</h3>
+                            </div>
+                            <div class="card-body">
+                                <strong>Name:</strong>
+                                <p class="text-muted">
+                                    <i class="fa fa-envelope-open text-pink"></i>
+                                    {{$user->name.' '.$user->lastName}}
+                                </p>
+                                <strong>Email:</strong>
+                                <p class="text-muted">
+                                    <i class="fa fa-envelope-open text-purple"></i>
+                                    {{$user->email}}
+                                </p>
+                                <strong>Phone:</strong>
+                                <p class="text-muted">
+                                    <i class="fa fa-phone text-success"></i>
+                                    {{$user->phone}}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
